e8087a4c3c82739e1b778341fa7fda2e0f48f194
Marcelo Tambalo <marcelo.zokis@gmail.com>
2013-03-14 13:57:46 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+CLASSIFIERS = [
+    'Framework :: Django',
+    'Environment :: Web Environment',
+    'Programming Language :: Python',
+    'Programming Language :: JavaScript',
+    'Natural Language :: Portuguese (Brazilian)'
+    'Intended Audience :: Developers',
+    'Topic :: Utilities',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+]
+
+setup(
+    author="Marcelo Fonseca Tambalo",
+    author_email="marcelo.zokis@gmail.com",
+    name='django-bootstrap-datetimepicker',
+    description='DateTime Picker to Django using Bootstrap Twitter and bootstrap-datetimepicker',
+    url='https://github.com/zokis/django-bootstrap-datetimepicker',
+    license='WTFPL',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'Django>=1.4',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
