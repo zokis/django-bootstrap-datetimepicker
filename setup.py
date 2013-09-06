cd6b8071e676892a87856bf2435936d7186bed2e
Marcelo Tambalo <marcelo.zokis@gmail.com>
2013-09-06 11:43:30 -0300

    update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ CLASSIFIERS = [
 setup(
     author="Marcelo Fonseca Tambalo",
     author_email="marcelo.zokis@gmail.com",
-    version='0.1',
+    version='0.2',
     name='django-bootstrap-datetimepicker',
     description='DateTime Picker to Django using Bootstrap Twitter and bootstrap-datetimepicker',
     url='https://github.com/zokis/django-bootstrap-datetimepicker',
