f88f14a64b3c791672ffa7ea900ce13f220cbed8
Ana Clara <clara.aclr@gmail.com>
2015-04-09 15:49:33 -0300

    add timepicker

---
+++ django_bootstrap_datetimepicker/static/datepicker/js/init_timepicker.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('div.bootstrap-timepicker > input').timepicker({
+        upArrowStyle: "fa fa-sort-asc",
+        downArrowStyle: "fa fa-sort-desc",
+        showMeridian: false,
+    });
+});
